3f1bb758f521272096acdacaf69809418b40903a
Dmitry Bobryshev <dmitry.bobryshev@brackets.sk>
2023-06-04 01:00:36 +0200

    fix(global): refactored code

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,19 +1,7 @@
 import Link from 'next/link';
 import { ThemeSwitcher } from '../ui/buttons/ThemeSwitcher';
-import { useDispatch, useSelector } from 'react-redux';
-import { themeSelector } from '../../redux/theme/selectors';
-import { setTheme } from '../../redux/theme/actionCreators';
 
 export const Header: React.FC = () => {
-  const theme = useSelector(themeSelector);
-
-  const dispatch = useDispatch();
-
-  function switchTheme() {
-    const nextTheme = theme === '' ? 'dark' : '';
-    dispatch(setTheme(nextTheme));
-  }
-
   return (
     <header className="sticky top-0 dark:bg-slate-800 bg-white border-b dark:border-slate-700 py-6">
       <div className="container mx-auto">
@@ -21,7 +9,7 @@ export const Header: React.FC = () => {
           <Link href="/" className="uppercase dark:text-white">
             Redux Dark Theme
           </Link>
-          <ThemeSwitcher onClick={switchTheme} />
+          <ThemeSwitcher />
         </div>
       </div>
     </header>
--- components/ui/buttons/ThemeSwitcher.tsx
+++ components/ui/buttons/ThemeSwitcher.tsx
@@ -1,14 +1,29 @@
-type TestProps = {
-  onClick?: () => void;
-};
+import { useDispatch, useSelector } from 'react-redux';
+import { themeSelector } from '../../../redux/theme/selectors';
+import { setTheme } from '../../../redux/theme/actionCreators';
+import Image from 'next/image';
 
-export const ThemeSwitcher: React.FC<TestProps> = ({ onClick }) => {
+export const ThemeSwitcher: React.FC = ({}) => {
+  const theme = useSelector(themeSelector);
+
+  const dispatch = useDispatch();
+
+  const switchTheme = () => {
+    const nextTheme = theme === '' ? 'dark' : '';
+    dispatch(setTheme(nextTheme));
+  };
+
   return (
     <button
-      onClick={onClick}
-      className="flex items-center justify-center text-center w-[42px] h-[42px] border border-slate-500 rounded-md dark:text-white"
+      onClick={switchTheme}
+      className="flex items-center justify-center text-center w-[42px] h-[42px] border dark:border-slate-700 rounded-md dark:text-white"
     >
-      D
+      <Image
+        src={`/assets/icons/${theme !== '' ? 'sun' : 'moon'}.svg`}
+        width={20}
+        height={20}
+        alt={`${theme !== '' ? 'Sun' : 'Moon'} icon`}
+      />
     </button>
   );
 };
